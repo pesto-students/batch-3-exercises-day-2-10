d98e540eb92a54292073484cbc55fecce1c3aa1b
Abhinav <abhinav95@hotmail.com>
2018-11-28 03:51:24 +0530

    Completed Day 2 Segment 1

--- your-name/yourName.test.js
+++ your-name/yourName.test.js
@@ -1,5 +1,7 @@
 import { yourName } from './yourName';
 
 describe('Write your name in the test', () => {
-  expect(yourName()).toBe('Write your name here!');
+  test('', () => {
+    expect(yourName()).toBe('Abhinav Kumar');
+  })
 });
