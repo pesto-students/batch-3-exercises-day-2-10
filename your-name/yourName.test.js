08475e64e81f6bf39ae98f0ad244c9e4c86ad165
Ankaj Goyal <a>
2018-11-28 03:55:23 +0530

    complete-day2-segmnt1

--- your-name/yourName.test.js
+++ your-name/yourName.test.js
@@ -1,5 +1,7 @@
 import { yourName } from './yourName';
 
 describe('Write your name in the test', () => {
-  expect(yourName()).toBe('Write your name here!');
+  test('should retur name', ()=>{
+    expect(yourName()).toBe('Ankaj Goyal');
+  });
 });
