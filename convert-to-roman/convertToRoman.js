8a3b32edcfca8d7e1c9daa153920b6173fda726f
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-29 03:48:45 +0530

    Convert to Roman fixes

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,42 +1,29 @@
 function convertToRoman(num) {
     if (isNaN(num)) return NaN;
-    var digits = String(+num).split(""),
-        key = [
-            "",
-            "C",
-            "CC",
-            "CCC",
-            "CD",
-            "D",
-            "DC",
-            "DCC",
-            "DCCC",
-            "CM",
-            "",
-            "X",
-            "XX",
-            "XXX",
-            "XL",
-            "L",
-            "LX",
-            "LXX",
-            "LXXX",
-            "XC",
-            "",
-            "I",
-            "II",
-            "III",
-            "IV",
-            "V",
-            "VI",
-            "VII",
-            "VIII",
-            "IX"
-        ],
-        roman = "",
-        i = 3;
-    while (i--) roman = (key[+digits.pop() + i * 10] || "") + roman;
-    return Array(+digits.join("") + 1).join("M") + roman;
+    var roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var str = "";
+
+    for (var i of Object.keys(roman)) {
+        var q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
 }
 
 export { convertToRoman };
