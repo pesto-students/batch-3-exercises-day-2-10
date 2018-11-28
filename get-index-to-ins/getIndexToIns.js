6f0dcc00795e0e4545b7fe4db70304bc4d14200e
Abhinav <abhinav95@hotmail.com>
2018-11-29 04:28:13 +0530

    Patch 2

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,12 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(arr, num) {
+  var counter;
+  arr.sort((a, b) => a - b);
+  for(var i = 0;i<arr.length;i++){
+    if(num - arr[i] <= 0)
+    return i;
+  }
+  return arr.length;
 }
 
 export {
