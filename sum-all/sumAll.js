c234f847eb41ea8687b8629f82a9539b298f3cc5
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-29 05:02:39 +0530

    Sum all

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,23 @@
+function sumAll(arr) {
+    if (arr[0] == arr[1]) return arr[0];
 
-function sumAll(...args) {
-  return args;
+    var start, end;
+
+    if (arr[0] > arr[1]) {
+        start = arr[0];
+        end = arr[1];
+    } else {
+        start = arr[1];
+        end = arr[0];
+    }
+
+    var sum = start + end;
+
+    for (var i = start + 1; i < end; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 }
 
-export {
-  sumAll,
-};
+export { sumAll };
