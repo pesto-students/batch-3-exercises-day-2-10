da6c574cc99d8f3fc951dade89ba574e78d83784
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-29 04:35:24 +0530

    Longest word in string

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,8 +1,15 @@
+function longestWordInString(str) {
+    var string = str.split(" ");
+    var longest = 0;
+    var word;
+    for (var i = 0; i <= string.length - 1; i++) {
+        if (longest < string[i].length) {
+            longest = string[i].length;
+            word = string[i];
+        }
+    }
 
-function longestWordInString(...args) {
-  return args;
+    return word.length;
 }
 
-export {
-  longestWordInString,
-};
+export { longestWordInString };
