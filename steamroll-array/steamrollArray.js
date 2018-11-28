df68cec33b408018e99a0ca5f850307519ad8f4f
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-29 04:50:39 +0530

    Steam roll array

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,8 +1,13 @@
-
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            newArr = newArr.concat(steamrollArray(arr[i]));
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 }
 
-export {
-  steamrollArray,
-};
+export { steamrollArray };
