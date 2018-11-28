5a130afb2eaad6c02366de7a4ad16731f0b56844
Ankaj Goyal <a>
2018-11-29 03:48:11 +0530

    Initial commit

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,18 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+
+function repeatStringNumTimes(a,b) {
+  if(a<=0)
+  {
+    return '';
+  }
+  else{
+    let str = '';
+    for(let i=0;i<b;i++){
+      
+       str = str + a;
+    }
+    return str;
+  }
 }
 
 export {
