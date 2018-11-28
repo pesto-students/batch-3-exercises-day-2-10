2b8dd36943a5aa8a9cca95c4dbfe4bf4f916232c
Abhinav <abhinav95@hotmail.com>
2018-11-29 05:02:21 +0530

    Patch 3

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,27 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(arrr) {
+  arrr = arrr.toLowerCase();
+  var arr = new Array();
+  for(var i=0;i<arrr.length;i++)
+    arr.push(arrr[i])
+
+  for(i =0;;i++){
+    if(i>= arr.length)
+    break;
+
+    if(arr[i]=== '-' || arr[i]=== '_' || arr[i]=== ' ' || arr[i]=== ',' || arr[i]=== '/'){
+      arr.splice(i,1);
+    }
+  }
+
+
+  for(i=0;i<=arr.length;i++){
+    if(arr[i] != arr[arr.length-i-1])
+    return false;
+  }
+
+  return true;
+  
 }
 
 export {
--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,17 @@
 
 function largeOfFour(...args) {
-  return args;
+
+  var arr = new Array();
+  args.forEach(element => {
+    var largest=element[0];
+    var len = element.length;
+    for(var i =1;i<length;i++){
+      if(element[i]>largest)
+      largest = element[i];
+    }
+    arr.push(largest);
+  });
+  return arr;
 }
 
 export {
