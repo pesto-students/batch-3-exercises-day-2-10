063799acc4f23859245dfc362e5ae6b14cf93aa4
Abhinav <abhinav95@hotmail.com>
2018-11-28 23:40:53 +0530

    Day 3 Segment 1

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,38 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator() {
+
+  if(arguments.length === 1)
+  return 0;
+  
+  if(arguments.length === 2)
+  return arguments[1];
+
+  var result =0;
+  switch (arguments[0]){
+    case '+':
+      for(var i = 1; i<arguments.length;i++)
+        var result = result + arguments[i];
+      break;
+
+    case '-':
+      result = arguments[1];
+      for(i=2;i<arguments.length;i++)
+        result = result - arguments[i];
+      break;
+
+    case '*':
+      result = 1;
+      for(i=1;i<arguments.length;i++)
+        result = result * arguments[i];
+      break;
+    case '/':
+    result = arguments[1];
+    for(i=2;i<arguments.length;i++)
+      result = result / arguments[i];
+    break;
+  }
+
+  return result;
 }
 
 export {
