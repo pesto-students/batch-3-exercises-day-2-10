4fee879e010c31e533f05903068d593c771cd1d8
Abhinav <abhinav95@hotmail.com>
2018-11-29 04:02:25 +0530

    Day 3 Segment 2

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,6 +1,15 @@
 
-function bouncer(...args) {
-  return args;
+function bouncer(arr) {
+  var arr2 = new Array();
+  for(var i=0;i<arr.length;i++){
+    if(arr[i] != '' && arr[i] != undefined && arr[i] != false && arr[i] != null && arr[i] != 0 ){
+      if(isNaN(arr[i]) && typeof(arr[i])==='number'){
+        continue;
+      }
+      arr2.push(arr[i]);
+    }
+  }
+  return arr2;
 }
 
 export {
