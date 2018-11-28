c786e96773fa7df75b759c2c8ace523b372f135c
Ankaj Goyal <a>
2018-11-29 04:51:30 +0530

    large of four

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,11 @@
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(a) {
+  var b = [];
+ for(var i of a ){
+   i.sort(function(a, b){return a - b});
+   b.push(i[3])
+ }
+ return b;
 }
 
 export {
