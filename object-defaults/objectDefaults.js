4c70e707e29ea5ba5ae0b12f86be1a057c5d154b
Abhinav <abhinav95@hotmail.com>
2018-11-30 00:31:19 +0530

    Object Dfault

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,16 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(...arg) {
+  if(arg.length === 1)
+  return arg[0];
+
+  var key;
+  arg.forEach(element => {
+    for(key in element){
+      if(!arg[0][key])
+        arg[0][key] = element[key];
+    }
+  });
+  return arg[0];
 }
 
 export {
