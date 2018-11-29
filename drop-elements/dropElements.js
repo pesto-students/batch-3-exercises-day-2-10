3a71e4edd65db0ff1bae49a5ebe63b881b9f39d5
Abhinav <abhinav95@hotmail.com>
2018-11-29 23:57:32 +0530

    Drop Element

--- drop-elements/dropElements.js
+++ drop-elements/dropElements.js
@@ -1,6 +1,12 @@
 
-function dropElements(...args) {
-  return args;
+function dropElements(arr,condition) {
+  var arr2 =[];
+  arr.forEach(element => {
+    if(condition(element))
+      arr2.push(element);
+    
+  });
+  return arr2;
 }
 
 export {
