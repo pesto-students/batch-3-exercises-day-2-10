55cf0feb46958a78aab9711ab704f64843de3ba9
Abhinav <abhinav95@hotmail.com>
2018-11-29 23:50:19 +0530

    Chunk In Array

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,19 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr,num) {
+  var arr2 = [];
+  var numOfOutputs = Math.ceil(arr.length/num);
+  
+  for(var i=0;;i++){
+    if(i+num <= arr.length)
+      arr2[i] = arr.splice(0,num);
+    else{
+      arr2[i] = arr.splice(0);
+      break;
+    }
+      
+  }
+
+  return arr2;  
 }
 
 export {
