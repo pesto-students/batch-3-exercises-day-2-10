e168c7abb60bed9df2a67c5c33bc2239b543540e
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-30 04:57:39 +0530

    Savings account fix

--- savings-account/savingsAccount.js
+++ savings-account/savingsAccount.js
@@ -2,15 +2,16 @@ function savingsAccount(accountNumber, email, firstName, lastName) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (accountNumber.length !== 6)
-        return "Account Number must be a 6-digit number";
+        throw new Error("Account Number must be a 6-digit number");
 
-    if (re.test(String(email).toLowerCase()) == false) return "Invalid e-mail";
+    if (re.test(String(email).toLowerCase()) == false)
+        throw new Error("Invalid e-mail");
 
     if (firstName.length < 3 || firstName.length > 20)
-        return "First name must be between 3 and 20 characters long";
+        throw new Error("First name must be between 3 and 20 characters long");
 
     if (!/^[a-z]+$/i.test(lastName))
-        return "Last name must contain english alphabets only";
+        throw new Error("Last name must contain english alphabets only");
 
     var details = {
         _accountNumber: accountNumber,
