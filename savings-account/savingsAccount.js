385984efbcb88b343e3dc2d0e65a6fdc2cc2c5ab
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-30 04:25:44 +0530

    Savings account

--- savings-account/savingsAccount.js
+++ savings-account/savingsAccount.js
@@ -1,8 +1,25 @@
+function savingsAccount(accountNumber, email, firstName, lastName) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-function savingsAccount(...args) {
-  return args;
+    if (accountNumber.length !== 6)
+        return "Account Number must be a 6-digit number";
+
+    if (re.test(String(email).toLowerCase()) == false) return "Invalid e-mail";
+
+    if (firstName.length < 3 || firstName.length > 20)
+        return "First name must be between 3 and 20 characters long";
+
+    if (!/^[a-z]+$/i.test(lastName))
+        return "Last name must contain english alphabets only";
+
+    var details = {
+        _accountNumber: accountNumber,
+        _email: email,
+        _firstName: firstName,
+        _lastName: lastName,
+        products: []
+    };
+    return details;
 }
 
-export {
-  savingsAccount,
-};
+export { savingsAccount };
