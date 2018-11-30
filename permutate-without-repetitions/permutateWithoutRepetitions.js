46bab8c56549748b25b2d7ec2dc0adaaf2179235
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-01 04:25:05 +0530

    Permutate without repetition

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,8 +1,12 @@
-
-function permutateWithoutRepetitions(...args) {
-  return args;
+function permutateWithoutRepetitions(arr) {
+  if (arr.length < 2) return [arr];
+  const res = [];
+  arr.forEach((x, i) => {
+    permutateWithoutRepetitions(arr.slice(0, i).concat(arr.slice(i + 1))).forEach((a) => {
+      res.push([x].concat(a));
+    });
+  });
+  return res;
 }
 
-export {
-  permutateWithoutRepetitions,
-};
+export { permutateWithoutRepetitions };
