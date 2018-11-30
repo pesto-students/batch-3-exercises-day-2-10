2a8fdd4315adccad71a4c1002d913df049c970c9
Ankaj Goyal <a>
2018-12-01 00:52:49 +0530

    addBig

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,13 @@
 
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(a) {
+  a = a.toString();
+  let b = a.split("\n").map(Number);
+  b[0] =0;
+  let sum = b.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0);
+  
+  return sum.toString();
 }
 
 export {
