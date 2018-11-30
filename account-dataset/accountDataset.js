3c0baf81d0b1c9739e9d008004325a9cac1c4e5f
Abhinav <abhinav95@hotmail.com>
2018-11-30 16:58:53 +0530

    Thousandier

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,35 @@
 
-function accountDataset(...args) {
-  return args;
+
+// function accountDataset(...args) {
+//   return args;
+// }
+
+function hundredThousandairs(...arg){
+  const dataset = require('./dataset.json');
+  var data = dataset['bankBalances'];
+  var arr = [];
+ 
+  data.forEach(element => {
+    
+    if(element['amount']>100000){
+      var state = element['state']; 
+      arr[state] = element['amount'];
+    }
+  });
+  return(arr);
 }
 
-export {
-  accountDataset,
-};
+hundredThousandairs();
+
+// export {
+//   accountDataset,
+//   hundredThousandairs,
+// };
+
+
+// {
+//   "bankBalances" : [
+//     {
+//       "amount": "822370.71",
+//       "state": "ME"
+//     },
