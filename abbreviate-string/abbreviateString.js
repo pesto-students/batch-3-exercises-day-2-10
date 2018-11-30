c77ff7a1c605582eefe20dd82624bc8a376d2150
Shikhar Dubey <shikhar.db@gmail.com>
2018-11-30 23:30:52 +0530

    Abbreviate String

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,9 @@
+function abbreviateString(str) {
+    let splittedString = str.split(" ");
+    let firstWord = splittedString[0];
+    let lastWord = splittedString[splittedString.length - 1][0].toUpperCase();
 
-function abbreviateString(...args) {
-  return args;
+    return `${firstWord} ${lastWord}.`;
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
