ffa42fed352c3e384bedd9b895cd377bb7d3e221
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-01 00:50:01 +0530

    Math Functions

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,17 @@
-
-function mathFns(...args) {
-  return args;
-}
-
-export {
-  mathFns,
+let squareRoot = () => {
+    return Math.sqrt(49);
 };
+
+let powerToTwo = () => {
+    return Math.pow(4, 2);
+};
+
+let roundOff = () => {
+    return Math.round(7.2);
+};
+
+let sqrt = squareRoot();
+let power = powerToTwo();
+let round = roundOff();
+
+export { sqrt, power, round };
