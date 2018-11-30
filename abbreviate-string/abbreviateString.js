115e2e3a1117d4fbf7d69e41edf323236479c411
Abhinav <abhinav95@hotmail.com>
2018-11-30 23:32:44 +0530

    abbreviate tring

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,22 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(txt) {
+  if(typeof txt != "string")
+    throw new Error("Invalid Parameter");
+    
+  const indexLast = txt.lastIndexOf(" ");
+  if(indexLast === -1){
+    return txt;
+  }
+
+  const indexFirst = txt.indexOf(" ");
+  
+
+  var subtxt1 = txt.substring(0,indexFirst+1);
+  var subtxt2 = txt[indexLast + 1].toUpperCase()+ '.';
+
+  return subtxt1+subtxt2;
+
+
 }
 
 export {
