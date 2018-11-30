5225691b8a7ab89359df33d50c711701b8a98504
Abhinav <abhinav95@hotmail.com>
2018-12-01 00:47:09 +0530

    MathFns

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,12 @@
 
-function mathFns(...args) {
-  return args;
+let sqrt = Math.sqrt(49);
+let power =Math.pow(2,4);
+let round =Math.round(6.9);
+
+export{
+  sqrt,
+  power,
+  round
 }
 
-export {
-  mathFns,
-};
+
