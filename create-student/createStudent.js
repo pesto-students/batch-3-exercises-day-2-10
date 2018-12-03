357d17a0488ab15c33488db6f8f864a2dcf5fdf5
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-03 23:32:13 +0530

    Create Student

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,8 +1,8 @@
-
-function createStudent(...args) {
-  return args;
+function createStudent({ likesES2015 = true, likesJavaScript = true } = {}) {
+  if (likesES2015 && !likesJavaScript) return 'The student likes ES2015!';
+  if (!likesES2015 && likesJavaScript) return 'The student likes JavaScript!';
+  if (!likesES2015 && !likesJavaScript) return 'The student does not like much...';
+  return 'The student likes JavaScript and ES2015';
 }
 
-export {
-  createStudent,
-};
+export { createStudent };
