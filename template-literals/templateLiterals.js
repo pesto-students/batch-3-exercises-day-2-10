0d8aedf06c558ae9457f13f130718c0387e5aa09
Abhinav <abhinav95@hotmail.com>
2018-12-04 02:03:21 +0530

    Template Literals

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,8 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const str1 = 'There are 4 people on the football team.';
+  const str2 = 'Their names are Rooney, Ronaldo, Messi, Pogba.';
+  return `${str1} ${str2}`;
 }
 
 export {
