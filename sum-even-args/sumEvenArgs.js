d8e3512dd9063d5d5e5c0a9bcdcd0ba155d33dac
Abhinav <abhinav95@hotmail.com>
2018-12-04 01:53:08 +0530

    Sum Even Args

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,8 +1,11 @@
-
 function sumEvenArgs(...args) {
-  return args;
+  let index = 0;
+  const sumOfEven = args.reduce((acc, val) => {
+    index += 1;
+    if (index % 2 === 0) return acc + val;
+    return acc + 0;
+  }, 0);
+  return sumOfEven;
 }
 
-export {
-  sumEvenArgs,
-};
+export { sumEvenArgs };
