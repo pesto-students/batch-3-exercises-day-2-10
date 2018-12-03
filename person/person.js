fffd82d986c8ea65c3c7055cde965bfd2e6b5751
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-04 05:06:55 +0530

    Person class

--- person/person.js
+++ person/person.js
@@ -1,8 +1,17 @@
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 
-function person(...args) {
-  return args;
+  addDobDigits() {
+    return this.dateOfBirth
+      .split('')
+      .filter(num => parseInt(num, 10))
+      .map(num => parseInt(num, 10))
+      .reduce((agg, num) => agg + num);
+  }
 }
 
-export {
-  person,
-};
+export { Person };
