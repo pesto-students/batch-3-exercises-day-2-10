4d4e6eb54f5be94acaa9c9cee8797ad223797ac1
Abhinav <abhinav95@hotmail.com>
2018-12-04 04:51:06 +0530

    Sequential Promise

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,8 +1,17 @@
-
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(args) {
+  if (args.length === 3) {
+    return Promise.resolve(
+      args[0]()
+        .then(res => args[1](res))
+        .then(res => args[2](res))
+        .then(res => res),
+    );
+  }
+  return Promise.resolve(
+    args[0]()
+      .then(res => args[1](res))
+      .then(res => res),
+  );
 }
 
-export {
-  sequentialPromise,
-};
+export { sequentialPromise };
