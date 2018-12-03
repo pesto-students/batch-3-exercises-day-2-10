0f68529c6b3c82366580a975bae076f55b1aabf9
Abhinav <abhinav95@hotmail.com>
2018-12-03 23:49:46 +0530

    Double Odd

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,8 +1,20 @@
+function doubleOddNumbers(args) {
+  // const oddDoubled = args
+  //   .map((element) => {
+  //     if (element % 2 === 1) return element * 2;
+  //     return undefined;
+  //   })
+  //   .filter((element) => {
+  //     if (element) return element;
+  //   });
 
-function doubleOddNumbers(...args) {
-  return args;
+  const oddDoubled = [];
+  for (const key of args) {
+    const currentElement = args[key];
+    if (currentElement % 2 === 1) oddDoubled.push(currentElement * 2);
+  }
+
+  return oddDoubled;
 }
 
-export {
-  doubleOddNumbers,
-};
+export { doubleOddNumbers };
