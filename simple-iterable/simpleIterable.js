2995f1d4bdd22db6819092f87650ef49cd03c9a2
Abhinav <abhinav95@hotmail.com>
2018-12-04 23:11:51 +0530

    Simple Iterator

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,8 +1,16 @@
-
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  let val = 0;
+  const iterable = {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (val > 4) return { value: undefined, done: true };
+      val += 1;
+      return { value: val, done: false };
+    },
+  };
+  return iterable;
 }
 
-export {
-  simpleIterable,
-};
+export { simpleIterable };
