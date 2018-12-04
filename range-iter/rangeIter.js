f112a867828d59ce21d570558bbc9cab6fcefc43
Abhinav <abhinav95@hotmail.com>
2018-12-04 23:29:28 +0530

    Range Iterable

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,8 +1,30 @@
-
-function rangeIter(...args) {
-  return args;
+function simpleIterable(first, last) {
+  let val = first - 1;
+  const iterable = {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (val > last - 1) return { value: undefined, done: true };
+      val += 1;
+      return { value: val, done: false };
+    },
+  };
+  return iterable;
 }
 
-export {
-  rangeIter,
-};
+function rangeIter(...args) {
+  if (!args[0]) throw new TypeError('undefined is not a number');
+  if (!args[1]) throw new TypeError('undefined is not a number');
+  if (typeof args[1] !== 'number') throw new TypeError('a is not a number');
+
+  if (args[1] < args[0]) return [];
+
+  const arr = [];
+  for (const val of simpleIterable(...args)) {
+    arr.push(val);
+  }
+  return arr;
+}
+
+export { rangeIter };
