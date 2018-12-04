6e1e687cedd81da6932ad94fee1e7d95a143c9b0
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-05 04:33:40 +0530

    Args string

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,8 +1,12 @@
-
-function argsString(...args) {
-  return args;
+function argsString(arg1, arg2) {
+  const bracesCount = (arg1.match(/{}/g) || []).length;
+  let str = arg1;
+  let i = 0;
+  while (i < bracesCount) {
+    str = str.replace('{}', arg2[i]);
+    i += 1;
+  }
+  return str;
 }
 
-export {
-  argsString,
-};
+export { argsString };
