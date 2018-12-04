cef7506b01e7d8d6ab05b6e742097e0a1d79b44d
Ankaj Goyal <a>
2018-12-04 23:20:36 +0530

    Initial Commit

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,20 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  return {
+    [Symbol.iterator]() {
+      let val = 0;
+      const iterator = {
+        next() {
+          val += 1;
+          if (val > 5) {
+            return { value: undefined, done: true };
+          }
+          return { value: val, done: false };
+        },
+      };
+      return iterator;
+    },
+  };
 }
 
 export {
