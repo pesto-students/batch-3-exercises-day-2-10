9743c4f8f40aa9a15266c7b77ff85e3594972d98
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-04 23:30:41 +0530

    Simple iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,8 +1,17 @@
-
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  return {
+    [Symbol.iterator]() {
+      let value = 0;
+      const iterator = {
+        next() {
+          value += 1;
+          if (value > 5) return { value: undefined, done: true };
+          return { value, done: false };
+        },
+      };
+      return iterator;
+    },
+  };
 }
 
-export {
-  simpleIterable,
-};
+export { simpleIterable };
