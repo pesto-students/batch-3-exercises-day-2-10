016d5868921e91c924c673fba82237bdff2cfe1f
Ankaj Goyal <a>
2018-12-05 03:32:23 +0530

    InitialCommit

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,14 @@
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(a) {
+  let obj = {};
+  for (let i of a) {
+    let cuberoot = Math.cbrt(i);
+    if (isNaN(cuberoot)||i===null) {
+      throw new Error('');
+    }
+    obj[i] = Math.cbrt(i);
+  }
+  return obj;
 }
 
 export {
