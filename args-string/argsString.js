dafa99c5dfc5197c5fbf1dbcbe0547a59923cd9d
Abhinav <abhinav95@hotmail.com>
2018-12-05 04:49:21 +0530

    argsString

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,8 +1,9 @@
-
-function argsString(...args) {
-  return args;
+function argsString(args, arr) {
+  let txt = args;
+  const regex = /{}/g;
+  const numOfOccurence = (args.match(regex) || []).length;
+  for (let counter = 0; counter < numOfOccurence; counter += 1) txt = txt.replace('{}', arr[counter]);
+  return txt;
 }
 
-export {
-  argsString,
-};
+export { argsString };
