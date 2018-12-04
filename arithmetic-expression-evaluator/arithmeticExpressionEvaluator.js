c5e865366ebb72ebef5a391f3ef74a4d20814507
Abhinav <abhinav95@hotmail.com>
2018-12-05 04:33:29 +0530

    Arithmetic Expression Evaluator

--- arithmetic-expression-evaluator/arithmeticExpressionEvaluator.js
+++ arithmetic-expression-evaluator/arithmeticExpressionEvaluator.js
@@ -1,8 +1,9 @@
-
-function arithmeticExpressionEvaluator(...args) {
-  return args;
+function arithmeticExpressionEvaluator(args) {
+  const regex = /[().^]/g;
+  if (args.match(regex)) throw new Error();
+  if (typeof args !== 'string') throw new Error();
+  // eslint-disable-next-line no-eval
+  return eval(args);
 }
 
-export {
-  arithmeticExpressionEvaluator,
-};
+export { arithmeticExpressionEvaluator };
