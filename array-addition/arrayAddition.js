4b343460e134d4d21a550b173d7572fb21cd69bf
Abhinav <abhinav95@hotmail.com>
2018-12-05 04:14:35 +0530

    Array Addition

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,8 +1,19 @@
+function arrayAddition(array1, array2) {
+  if (array1.length === 0 || array2.length === 0) throw new Error();
 
-function arrayAddition(...args) {
-  return args;
+  let arr1 = array1;
+  let arr2 = array2;
+
+  if (arr2.length > arr1.length) {
+    arr2 = array1;
+    arr1 = array2;
+  }
+  const sum = arr1.map((value, index) => {
+    if (typeof value === 'string' || typeof arr2[index] === 'string') throw new Error();
+    return (value || 0) + (arr2[index] || 0);
+  });
+
+  return sum;
 }
 
-export {
-  arrayAddition,
-};
+export { arrayAddition };
