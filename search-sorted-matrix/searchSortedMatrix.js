eaa6f4f69266cba49f85718ace0135738805d73c
Ankaj Goyal <a>
2018-12-06 03:49:06 +0530

    Initial Commit

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,15 @@
 
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix(obj) {
+  let bool = false;
+  const { search, matrix } = obj;
+  for (const i of matrix) {
+    for (const j of i) {
+      if (j === search) {
+        bool = true;
+      }
+    }
+  }
+  return bool;
 }
 
 export {
