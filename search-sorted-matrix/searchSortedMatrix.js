49d3e74aa22ef4bb1554430e58b78e926ca790db
Abhinav <abhinav95@hotmail.com>
2018-12-06 04:48:02 +0530

    First Commit

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,8 +1,21 @@
-
-function searchSortedMatrix(...args) {
-  return args;
+function searchFirstVertical(obj) {
+  const { search, matrix } = obj;
+  for (let counter = 0; ; counter += 1) {
+    if (matrix[counter][0] === search) return { index: counter, found: true };
+    if (matrix[counter][0] > search) return { index: counter - 1, found: false };
+  }
+}
+function searchSortedMatrix(obj) {
+  const { search, matrix } = obj;
+  const { index, found } = searchFirstVertical(obj);
+  if (found) return true;
+  if (Number.isNaN(index)) return false;
+  // search horizontal
+  for (let counter = 0; ; counter += 1) {
+    if (matrix[index][counter] === search) return true;
+    if (Number.isNaN(matrix[index][counter])) break;
+  }
+  return false;
 }
 
-export {
-  searchSortedMatrix,
-};
+export { searchSortedMatrix };
--- slice/slice.js
+++ slice/slice.js
@@ -1,8 +1,12 @@
-
-function slice(...args) {
-  return args;
+function slice(frstIndex, lstIndex, arr) {
+  return arr.slice(frstIndex, lstIndex);
+  // let firstIndex = frstIndex;
+  // let lastIndex = lstIndex;
+  // const arrLength = arr.length;
+  // if (frstIndex < 0) firstIndex = frstIndex + arrLength;
+  // if (lstIndex < 0) lastIndex = lstIndex + arrLength;
+  // // return [firstIndex, lastIndex];
+  // return arr.slice(firstIndex, lastIndex);
 }
 
-export {
-  slice,
-};
+export { slice };
