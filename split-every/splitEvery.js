2d3beb500ad964329e7e8070fc4a88830d1ca5b2
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-06 04:11:50 +0530

    Split Every

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,12 @@
+function splitEvery(num, arr) {
+  if (num <= 0) throw new Error();
 
-function splitEvery(...args) {
-  return args;
+  const result = [];
+  for (let i = 0; i < arr.length; i += num) {
+    result.push(arr.slice(i, i + num));
+  }
+
+  return result;
 }
 
-export {
-  splitEvery,
-};
+export { splitEvery };
