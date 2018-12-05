f6101203cb906652f035c413f18eee4718dfbf43
Abhinav <abhinav95@hotmail.com>
2018-12-05 23:30:29 +0530

    knownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,11 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(obj) {
+  const myProxy = new Proxy(obj, {
+    get(tobj, key) {
+      if ({}.hasOwnProperty.call(tobj, key)) return tobj[key];
+      throw new TypeError('Unknown Property');
+    },
+  });
+  return myProxy;
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
