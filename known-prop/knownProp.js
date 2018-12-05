f5ea00821b0309fa3eaff4dfce09c8eef49f8a9e
Ankaj Goyal <a>
2018-12-05 23:19:13 +0530

    Initial Commit

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,16 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(target) {
+  const handler = {
+    get(tar, name) {
+      if (name in tar) {
+        return tar[name];
+      }
+      throw new TypeError('Unknown Property');
+    },
+  };
+  const p = new Proxy(target, handler);
+  return p;
 }
-
 export {
   knownProp,
 };
