9c7cc08f1e70d2447f1fb62996db4a265e969b30
Ankaj Goyal <a>
2018-12-07 00:17:21 +0530

    primality test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,16 @@
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(a) {
+  let bool = false;
+  if (a === 0 || a === 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(a); i += 1) {
+    if (a % i === 0) {
+      bool = true;
+      break;
+    }
+  }
+  return bool;
 }
 
 export {
