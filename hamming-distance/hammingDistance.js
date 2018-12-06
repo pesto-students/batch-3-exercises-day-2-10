5af0993807e7441a12f947e7518a158da9d62903
Abhinav <abhinav95@hotmail.com>
2018-12-07 00:10:57 +0530

    Debugging HTML, Hamming Ditance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,8 +1,10 @@
-
-function hammingDistance(...args) {
+function hammingDistance(val1, val2) {
+  if (val1.length !== val2.length) throw new Error();
+  const args = val1.split('').reduce((acc, value, ind) => {
+    if (value !== val2[ind]) return acc + 1;
+    return acc;
+  }, 0);
   return args;
 }
 
-export {
-  hammingDistance,
-};
+export { hammingDistance };
