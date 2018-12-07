0827c3187bb3a9f9ee8a21d1dd2e3b45d2b65856
Ankaj Goyal <a>
2018-12-08 00:17:52 +0530

    second commit

--- cartesian-product/cartesianProduct.js
+++ cartesian-product/cartesianProduct.js
@@ -1,6 +1,11 @@
 
-function cartesianProduct(...args) {
-  return args;
+function cartesianProduct(a, b) {
+  if (a.length === 0 || b === null) {
+    return null;
+  }
+  const c = [];
+  const res = a.map(i => b.map(j => c.concat(i, j)));
+  return res.flat();
 }
 
 export {
