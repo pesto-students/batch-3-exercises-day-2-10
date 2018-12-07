22bb84942f95b6dd749252b6a03fa330283eef74
Abhinav <abhinav95@hotmail.com>
2018-12-08 00:19:39 +0530

    IN Progress

--- counting-anagrams/countingAnagrams.js
+++ counting-anagrams/countingAnagrams.js
@@ -1,6 +1,31 @@
+function isAnagram(line1, line2) {
+  if (line1.length === 1 && line2.length === 1) return false;
+  if (line1.length !== line2.length) return false;
 
-function countingAnagrams(...args) {
-  return args;
+  const arrLine1 = line1.split('').sort();
+  const arrLine2 = line2.split('').sort();
+
+  for (const [index, val] of arrLine1) {
+    if (val !== arrLine2[index]) return false;
+  }
+
+  return true;
+}
+
+function countingAnagrams(line) {
+  const arr = line.split(' ');
+  let counter = 0;
+  const number = arr.reduce((acc, value) => {
+    arr.filter((val) => {
+      if (isAnagram(val, value)) {
+        counter += 1;
+      }
+      return false;
+    });
+
+    return acc + counter;
+  }, 0);
+  return (number / 2);
 }
 
 export {
--- levenshtein-distance/levenshteinDistance.js
+++ levenshtein-distance/levenshteinDistance.js
@@ -1,6 +1,15 @@
 
-function levenshteinDistance(...args) {
-  return args;
+function levenshteinDistance(line1, line2) {
+  if (line1.length === 0) return line2.length;
+  if (line2.length === 0) return line1.length;
+  const lengthDiff = line2.length - line1.length;
+  const substitutionNeeded = line1.split('').reduce((acc, val, index) => {
+    if (val !== line2[index]) {
+      return (acc + 1);
+    }
+    return acc;
+  }, 0);
+  return (substitutionNeeded + lengthDiff);
 }
 
 export {
